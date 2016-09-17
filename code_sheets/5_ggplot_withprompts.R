52a1288fa584b191e6da521c0a3ceecb5f021749
thegricean <judithdegen@gmail.com>
2016-09-17 09:02:44 -0400

    added ggplot sheet

---
+++ code_sheets/5_ggplot_withprompts.R
@@ -0,0 +1,13 @@
+# Using ggplot to visualize data
+# created by jdegen on Sep 17, 2016
+
+setwd("/Users/titlis/cogsci/teaching/_2016/mem_tutorial/")
+source("code_sheets/helpers.R")
+library(ggplot2)
+library(languageR)
+library(dplyr)
+library(magrittr)
+library(tidyr)
+
+# Which aspects of the lexdec dataset (or models thereof) do we want to visualize?
+
